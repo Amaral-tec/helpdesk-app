6a8a59b8f2ce2784d97ce96f99cc654776323bc0
Leandro Amaral <amaral_adm@hotmail.com>
2024-05-01 20:06:38 -0300

    Updates to the Customer and Technician classes

--- helpdesk/src/main/java/com/amaral/helpdesk/model/Client.java
+++ helpdesk/src/main/java/com/amaral/helpdesk/model/Client.java
@@ -9,6 +9,8 @@ import javax.persistence.OneToMany;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
+import com.amaral.helpdesk.enums.Profile;
+
 @Entity
 @Table(name = "clients")
 @PrimaryKeyJoinColumn(name = "id")
@@ -21,10 +23,12 @@ public class Client extends User {
 
 	public Client() {
 		super();
+		addProfile(Profile.CLIENT);
 	}
 
 	public Client(Long id, String name, String email, String phone, String cpf, Date dateBirth, String password) {
 		super(id, name, email, phone, cpf, dateBirth, password);
+		addProfile(Profile.CLIENT);
 	}
 
 	public List<Ticket> getTickets() {
